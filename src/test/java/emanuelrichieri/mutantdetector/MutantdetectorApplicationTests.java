62b510322255858f2d84b47004d4a35c1677654e
emanuelrichieri <emanuel.richieri@gmail.com>
2020-08-15 11:52:22 -0300

    Init project

---
+++ src/test/java/emanuelrichieri/mutantdetector/MutantdetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package emanuelrichieri.mutantdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantdetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
